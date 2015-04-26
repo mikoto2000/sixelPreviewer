912fed51f596e0558276d9c4f46cef149189f459
mikoto2000 <mikoto2000@gmail.com>
2015-04-26 23:43:27 +0000

    Initial commit.

---
+++ printImg.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 第一引数に指定した画像を、
+# 第二引数の PTS で表示する。
+#
+# Useag:
+# printImg TARGET_IMG_FILE TARGET_PTS_PATH
+
+run_tty=${2}
+
+ext=${1##*.}
+out_scale=0.9
+
+read -r rows char_of_row term_width term_height <<< "`termsize.py ${run_tty}`"
+
+if [ "${ext}" = "svg" ]; then
+    convert ${1} /tmp/tmp.png
+elif [ "${ext}" = "ozcld" ]; then
+    ozcld ${1} > /tmp/tmp.dot
+    dot -T png /tmp/tmp.dot -o /tmp/tmp.png
+fi
+
+read -r image_width image_height <<< `identify -format "%w %h" /tmp/tmp.png`
+
+w_scale=`echo "${term_width}/${image_width}" | bc`
+h_scale=`echo "${term_height}/${image_height}" | bc`
+
+if [ $w_scale -lt $h_scale ]; then
+sed s/\.[0-9,]*$//g
+    size_opt="--width=`echo \"${term_width}*${out_scale}\" | bc | sed s/\.[0-9,]*$//g`"
+else
+    size_opt="--height=`echo \"${term_height}*${out_scale}\" | bc | sed s/\.[0-9,]*$//g`"
+fi
+
+img2sixel ${size_opt} /tmp/tmp.png > ${run_tty}
---
+++ termsize.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Get terminal size.
+"""
+
+__author__ = "mikoto2000 <mikoto2000@gmail.com>"
+__version__ = "1.0.0"
+__date__ = "22 Apr 2015"
+
+__USEAGE__ = """Useage: termsize.py PATH_TO_PTS"""
+
+import struct,fcntl,termios,sys;
+
+"""
+Get terminal size.
+
+Return terminal size array.
+[0] : line count.
+[1] : charactor count of line.
+[2] : pixels of width.
+[3] : pixels of height.
+"""
+def getTermSize(ptsPath) :
+    termSize=struct.unpack(
+        'HHHH',
+        fcntl.ioctl(
+            open(ptsPath),
+            termios.TIOCGWINSZ,
+            '\0' * 8))
+    return termSize
+
+if __name__ == "__main__" :
+
+    if len(sys.argv) != 2 :
+        print __USEAGE__
+        exit()
+
+    if sys.argv[1] == "--help"\
+        or sys.argv[1] == "-h" :
+        print __USEAGE__
+        exit()
+
+    termSize = getTermSize(sys.argv[1])
+    print termSize[0], termSize[1], termSize[2], termSize[3]
