99925ae902561c52c69684a6c97e65e0649db149
mikoto2000 <mikoto2000@gmail.com>
2015-05-21 23:38:23 +0000

    Modified work directory.

--- printImg.sh
+++ printImg.sh
@@ -8,21 +8,25 @@
 
 run_tty=${2}
 
+# 作業ディレクトリ作成
+work_dir=/tmp/sixelPreviewer
+mkdir -p ${work_dir}
+
 ext=${1##*.}
 out_scale=0.9
 
 read -r rows char_of_row term_width term_height <<< "`termsize.py ${run_tty}`"
 
 if [ "${ext}" = "png" -o "${ext}" = "jpg" -o "${ext}" = "gif" ]; then
-    cp ${1} /tmp/tmp.${ext}
+    cp ${1} ${work_dir}/tmp.${ext}
 elif [ "${ext}" = "svg" ]; then
-    convert ${1} /tmp/tmp.png
+    convert ${1} ${work_dir}/tmp.png
 elif [ "${ext}" = "ozcld" ]; then
-    ozcld ${1} > /tmp/tmp.dot
-    dot -T png /tmp/tmp.dot -o /tmp/tmp.png
+    ozcld ${1} > ${work_dir}/tmp.dot
+    dot -T png ${work_dir}/tmp.dot -o ${work_dir}/tmp.png
 fi
 
-read -r image_width image_height <<< `identify -format "%w %h" /tmp/tmp.png`
+read -r image_width image_height <<< `identify -format "%w %h" ${work_dir}/tmp.png`
 
 w_scale=`echo "${term_width}/${image_width}" | bc`
 h_scale=`echo "${term_height}/${image_height}" | bc`
@@ -33,4 +37,4 @@ else
     size_opt="--height=`echo \"${term_height}*${out_scale}\" | bc | sed s/\.[0-9,]*$//g`"
 fi
 
-img2sixel ${size_opt} /tmp/tmp.png > ${run_tty}
+img2sixel ${size_opt} ${work_dir}/tmp.png > ${run_tty}
